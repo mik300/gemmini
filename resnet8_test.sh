60b4542de42b108ba34cdb0a59302f4e8c345079
mik300 <emichael283@gmail.com>
2024-10-21 17:29:37 +0200

    Modified resnet8.sh

--- resnet8_test.sh
+++ resnet8_test.sh
@@ -6,39 +6,48 @@ if [ $# -ne 1 ]; then
 fi
 
 
-file_path1="software/gemmini-rocc-tests/bareMetalC/conv_layer.c"
 
 
 
-
-layers_list=(conv_1 layer1_0_conv1 layer1_0_conv2 layer2_0_conv1 layer2_0_conv2 layer3_0_conv1 layer3_0_conv2)
-scale=(157 276 155 221 304 382 452)
+layers_list=(conv_1 layer1_0_conv1 layer1_0_conv2 layer2_0_conv1 layer2_0_conv2 layer3_0_conv1 layer3_0_conv2 linear)
+scale=(157 276 155 221 304 382 452 49)
 # For loop to run the script for all layers of resnet8
-for ((i=0; i<7; i++))
+for ((i=0; i<8; i++))
 do
-    # Modify the layer name in conv_layer.c
-    echo "${layers_list[$i]}"
-    python change_layer.py software/gemmini-rocc-tests/bareMetalC/conv_layer.c "${layers_list[$i]}"
 
+    echo "${layers_list[$i]}"
+    if [ "$i" -eq 7 ]; then
+        file_path="software/gemmini-rocc-tests/bareMetalC/linear_layer.c"
+    else 
+        file_path="software/gemmini-rocc-tests/bareMetalC/conv_layer.c"
+        # Modify the layer name in conv_layer.c
+        python change_layer.py software/gemmini-rocc-tests/bareMetalC/conv_layer.c "${layers_list[$i]}"
+    fi
+
+    
     #if it's layer layer2_0_conv1 or layer3_0_conv1 then STRIDE should be set to 2
     if [[ "$i" -eq 3 || "$i" -eq 5 ]]; then
-        sed -i "s/const int STRIDE = [0-9]\+/const int STRIDE = 2/" "$file_path1"
+        sed -i "s/const int STRIDE = [0-9]\+/const int STRIDE = 2/" "$file_path"
     else 
-        sed -i "s/const int STRIDE = [0-9]\+/const int STRIDE = 1/" "$file_path1"
+        sed -i "s/const int STRIDE = [0-9]\+/const int STRIDE = 1/" "$file_path"
     fi
 
     # Set scale depending on the layer
-    sed -i "s/NO_ACTIVATION, 1.0 \/ [0-9]\+, 0, 0, 0,/NO_ACTIVATION, 1.0 \/ ${scale[$i]}, 0, 0, 0,/" "$file_path1"
+    sed -i "s/NO_ACTIVATION, 1.0 \/ [0-9]\+,/NO_ACTIVATION, 1.0 \/ ${scale[$i]},/" "$file_path"
 
     #if linear layer set BIAS to true
     if [ "$i" -eq 7 ]; then
-        sed -i "s/const int NO_BIAS = [0-9]\+/const int NO_BIAS = 0/" "$file_path1"
+        sed -i "s/const int NO_BIAS = [0-9]\+/const int NO_BIAS = 0/" "$file_path"
+        ./scripts/run-"$1".sh linear_layer > tmp.txt
+        if [ $? -ne 0 ]; then
+            echo "Error: Predictions don't match, compare gemmini_output.txt and output_mat_8.txt for details."
+            echo "Script terminated."
+            exit 1  # Terminate the bash script with an error status
+        fi
     else 
-        sed -i "s/const int NO_BIAS = [0-9]\+/const int NO_BIAS = 1/" "$file_path1"
+        sed -i "s/const int NO_BIAS = [0-9]\+/const int NO_BIAS = 1/" "$file_path"
+        ./scripts/run-"$1".sh conv_layer > tmp.txt
     fi
-
-    # This script can only be ran with verilator. spike simply doesn't work with the appr multiplier.
-    ./scripts/run-spike.sh conv_layer > tmp.txt 2>/dev/null
 
     # Extract output_mat and save it in gemmini_output.txt
     ./clean_output.sh tmp.txt
@@ -46,7 +55,7 @@ do
     errors=$(python evaluate_results.py --output_mat "$i")    
     echo "$errors"
     echo ""
+
 done
-#rm output.txt conv
-echo "Campaign is done."
+echo "Predictions match"
 
