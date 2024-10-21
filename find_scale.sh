bff75773c387b7f488b01a0c3a96097dcc35b4c2
mik300 <emichael283@gmail.com>
2024-10-21 14:31:54 +0200

    Modified find_scale.sh to work with linear layer

--- find_scale.sh
+++ find_scale.sh
@@ -18,38 +18,48 @@ get_index() {
     echo "-1"  # Return -1 if not found
 }
 
-file_path1="software/gemmini-rocc-tests/bareMetalC/conv_layer.c"
+if [ "$1" != "linear" ]; then
+    file_path="software/gemmini-rocc-tests/bareMetalC/conv_layer.c"
+else
+    file_path="software/gemmini-rocc-tests/bareMetalC/linear_layer.c"
+fi
 
 #if it's layer layer2_0_conv1 or layer3_0_conv1 then STRIDE should be set to 2
 if [[ "$1" == "layer2_0_conv1" || "$1" == "layer3_0_conv1" ]]; then
-    sed -i "s/const int STRIDE = [0-9]\+/const int STRIDE = 2/" "$file_path1"
+    sed -i "s/const int STRIDE = [0-9]\+/const int STRIDE = 2/" "$file_path"
 else 
-    sed -i "s/const int STRIDE = [0-9]\+/const int STRIDE = 1/" "$file_path1"
+    sed -i "s/const int STRIDE = [0-9]\+/const int STRIDE = 1/" "$file_path"
 fi
 
-python change_layer.py software/gemmini-rocc-tests/bareMetalC/conv_layer.c "$1"
+if [ "$1" != "linear" ]; then
+    python change_layer.py software/gemmini-rocc-tests/bareMetalC/conv_layer.c "$1"
+fi
+
 
 # For loop to run the script with multiple values of approximate level
-for ((i=450; i<=460; i+=1))
+for ((i=45; i<=51; i+=1))
 do
     
-    sed -i "s/NO_ACTIVATION, 1.0 \/ [0-9]\+, 0, 0, 0,/NO_ACTIVATION, 1.0 \/ $i, 0, 0, 0,/" "$file_path1"
+    sed -i "s/NO_ACTIVATION, 1.0 \/ [0-9]\+,/NO_ACTIVATION, 1.0 \/ $i,/" "$file_path"
 
     # This script can only be ran with verilator. spike simply doesn't work with the appr multiplier.
-    ./scripts/run-spike.sh conv_layer > tmp.txt 2>/dev/null
+    if [ "$1" != "linear" ]; then
+       ./scripts/run-spike.sh conv_layer > tmp.txt 2>/dev/null
+    else
+       ./scripts/run-spike.sh linear_layer > tmp.txt 2>/dev/null
+    fi
+    
 
     # Extract output_mat and save it in gemmini.output.txt
     ./clean_output.sh tmp.txt
 
     index=$(get_index "$1")
-    # Compile and run the golden model which is convolution.c, output_mat is stored in CPU_output.txt
-    mse=$(python evaluate_results.py --output_mat "$index")
 
-    echo "scale = $i"
-    echo "MSE = $mse"
+    # Compile and run the golden model which is convolution.c, output_mat is stored in CPU_output.txt
+    errors=$(python evaluate_results.py --output_mat "$index")
+
+    echo "scale = $i"   
+    echo "$errors"
     echo ""
 
 done
-#rm output.txt conv
-
-
